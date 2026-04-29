e02f315fe4c6c34fa17804f7a610216380e49ac9
estebancif <estebancifuentes.b69@gmail.comf>
2026-04-29 18:23:28 +0200

    configuracion de app.routes.ts

--- web-coches/src/app/app.routes.ts
+++ web-coches/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ModelosComponent } from './pages/modelos/modelos.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'modelos', component: ModelosComponent },
+  { path: 'noticias', component: NoticiasComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '' }
+];
