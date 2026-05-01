e2f984492ac42f9c64d6fc05df6dd754da1c22a3
estebancif <estebancifuentes.b69@gmail.comf>
2026-05-01 21:09:42 +0200

    creacion de home

--- web-coches/src/app/app.routes.ts
+++ web-coches/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './componentes/home/home.component';
-import { ModelosComponent } from './paginas/modelos/modelos.component';
+
+import { HomeComponent } from './paginas/home/home.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
 import { NoticiasComponent } from './paginas/noticias/noticias.component';
+import { ModelosComponent } from './paginas/modelos/modelos.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'modelos', component: ModelosComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'noticias', component: NoticiasComponent }
