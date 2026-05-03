4022b598c55c2f08a9f2ad5d92f13dd2548ce6b9
estebancif <estebancifuentes.b69@gmail.comf>
2026-05-03 20:40:51 +0200

    Cargar noticias desde JSON

--- web-coches/src/app/servicios/noticias.service.ts
+++ web-coches/src/app/servicios/noticias.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Noticia {
+  fecha: string;
+  titulo: string;
+  imagen?: string;
+  video?: string;
+  resumen: string;
+  contenido: string;
+}
+
+interface NoticiasResponse {
+  noticias: Noticia[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiasService {
 
-  constructor() { }
+  private noticiasUrl = 'assets/data/noticias.json';
+
+  constructor(private http: HttpClient) { }
+
+  obtenerNoticias(): Observable<NoticiasResponse> {
+    return this.http.get<NoticiasResponse>(this.noticiasUrl);
+  }
 }
